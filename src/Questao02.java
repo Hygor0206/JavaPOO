d774890ec4ad874a94b172ffcd9c5c30c02c4f75
Hygor Marques <hygor.marques@protonmail.com>
2023-08-11 09:23:42 -0300

    Question 02 finished

--- src/Questao02.java
+++ src/Questao02.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Questao02 {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Qual o valor do salario minimo?");
+        double vlrSalarioMinimo = input.nextDouble();
+        System.out.println("Quantos quilowatt foi consumido na residencia?");
+        double quantidadekW = input.nextDouble();
+        System.out.printf("Conforme o salario minimo informado, cada kW custa em torno de: %.2f\n", (vlrSalarioMinimo*0.01));
+        System.out.printf("A residencia deve pagar um total de %.2f pela quantidade de kW utilizada", ((vlrSalarioMinimo*0.01)*quantidadekW)); 
     }
 }
