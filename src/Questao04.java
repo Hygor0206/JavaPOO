e07c33a6fe754415491b2a916a3d5e2c89822285
Hygor Marques <hygor.marques@protonmail.com>
2023-08-14 10:06:06 -0300

    Improving code

--- src/Questao04.java
+++ src/Questao04.java
@@ -4,45 +4,38 @@ public class Questao04 {
     public static boolean range(int x, int min, int max){
         return min <= x && x<=max;
     }
+
+    public static double calcValor(int quantidade, double valor){
+        double valorTotal=0;
+        if(quantidade*valor<250){
+            valorTotal=(quantidade*valor)-((quantidade*valor)*0.05);
+        }else if(quantidade*valor<500){
+            valorTotal=(quantidade*valor)-((quantidade*valor)*0.10);
+        }else{
+            valorTotal=(quantidade*valor)-((quantidade*valor)*0.15);
+        }
+        return valorTotal;
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Qual o codigo do protuto comprado?");
+        System.out.println("Qual o codigo do produto comprado?");
         int codigo = input.nextInt();
-        System.out.printf("Quantos protudos de código %d foram comprados?\n", codigo);
-        double quantidade = input.nextInt();
+        System.out.printf("Quantos produdos de codigo %d foram comprados?\n", codigo);
+        int quantidade = input.nextInt();
         if(range(codigo, 1, 10)){
-            if(quantidade*10<250){
-                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*10)-((quantidade*10)*0.05)));
-            }else if(quantidade*10<500){
-                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*10)-((quantidade*10)*0.10)));
-            }else{
-                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*10)-((quantidade*10)*0.15)));
-            }
+            double valorTotal=calcValor(quantidade, 10);
+            System.out.printf("O valor total será de R$%.2f\n", valorTotal);
         }else if(range(codigo, 11, 20)){
-            if(quantidade*15<250){
-                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*15)-((quantidade*15)*0.05)));
-            }else if(quantidade*15<500){
-                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*15)-((quantidade*15)*0.10)));
-            }else{
-                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*15)-((quantidade*15)*0.15)));
-            }
+            double valorTotal=calcValor(quantidade, 15);
+            System.out.printf("O valor total será de R$%.2f\n", valorTotal);
         }else if(range(codigo, 21, 30)){
-            if(quantidade*20<250){
-                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*20)-((quantidade*20)*0.05)));
-            }else if(quantidade*20<500){
-                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*20)-((quantidade*20)*0.10)));
-            }else{
-                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*20)-((quantidade*20)*0.15)));
-            }
+            double valorTotal=calcValor(quantidade, 20);
+            System.out.printf("O valor total será de R$%.2f\n", valorTotal);
         }else if(range(codigo, 31, 40)){
-            if(quantidade*40<250){
-                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*40)-((quantidade*40)*0.05)));
-            }else if(quantidade*40<500){
-                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*40)-((quantidade*40)*0.10)));
-            }else{
-                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*40)-((quantidade*40)*0.15)));
-            }
+            double valorTotal=calcValor(quantidade, 40);
+            System.out.printf("O valor total será de R$%.2f\n", valorTotal);
         }else{
             System.out.println("Este codigo de protudo nao existe");
         }
