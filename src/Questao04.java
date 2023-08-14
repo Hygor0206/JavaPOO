e5ce24cfdb1607783a10a93b079f77c22c63ae59
Hygor Marques <hygor.marques@protonmail.com>
2023-08-14 09:52:54 -0300

    Added the discount prices

--- src/Questao04.java
+++ src/Questao04.java
@@ -12,13 +12,37 @@ public class Questao04 {
         System.out.printf("Quantos protudos de código %d foram comprados?\n", codigo);
         double quantidade = input.nextInt();
         if(range(codigo, 1, 10)){
-            System.out.printf("O valor total será de R$%.2f\n", (quantidade*10));
+            if(quantidade*10<250){
+                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*10)-((quantidade*10)*0.05)));
+            }else if(quantidade*10<500){
+                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*10)-((quantidade*10)*0.10)));
+            }else{
+                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*10)-((quantidade*10)*0.15)));
+            }
         }else if(range(codigo, 11, 20)){
-            System.out.printf("O valor total será de R$%.2f\n", (quantidade*15));
+            if(quantidade*15<250){
+                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*15)-((quantidade*15)*0.05)));
+            }else if(quantidade*15<500){
+                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*15)-((quantidade*15)*0.10)));
+            }else{
+                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*15)-((quantidade*15)*0.15)));
+            }
         }else if(range(codigo, 21, 30)){
-            System.out.printf("O valor total será de R$%.2f\n", (quantidade*20));
+            if(quantidade*20<250){
+                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*20)-((quantidade*20)*0.05)));
+            }else if(quantidade*20<500){
+                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*20)-((quantidade*20)*0.10)));
+            }else{
+                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*20)-((quantidade*20)*0.15)));
+            }
         }else if(range(codigo, 31, 40)){
-            System.out.printf("O valor total será de R$%.2f\n", (quantidade*40));
+            if(quantidade*40<250){
+                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*40)-((quantidade*40)*0.05)));
+            }else if(quantidade*40<500){
+                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*40)-((quantidade*40)*0.10)));
+            }else{
+                System.out.printf("O valor total será de R$%.2f\n", ((quantidade*40)-((quantidade*40)*0.15)));
+            }
         }else{
             System.out.println("Este codigo de protudo nao existe");
         }
---
+++ src/Questao05.java
@@ -0,0 +1,5 @@
+public class Questao05 {
+    public static void main(String[] args) {
+        
+    }
+}
