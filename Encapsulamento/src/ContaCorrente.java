8c3cbf9b92c9c60755baa5ca12c2999a7c614328
Hygor Marques <hygor.marques@protonmail.com>
2023-09-22 08:13:23 -0300

    Conta Corrente test

---
+++ Encapsulamento/src/ContaCorrente.java
@@ -0,0 +1,61 @@
+public class ContaCorrente {
+    private String nome;
+    private String cpf;
+    private double saldo;
+    private int numConta;
+    private int sequencial=0;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public int getNumConta() {
+        return this.numConta;
+    }    
+
+    private int gerarNumConta(){
+        return sequencial++;
+    }
+    
+    public ContaCorrente(String nome,String cpf){
+        setNome(nome);
+        this.cpf=cpf;
+        this.saldo=0;
+        this.numConta = gerarNumConta();
+    }
+
+    public ContaCorrente(String nome, String cpf, double saldo){
+        setNome(nome);
+        this.cpf=cpf;
+        this.saldo=saldo;
+        this.numConta = gerarNumConta();
+    }
+
+    public boolean sacar(double valor){
+        if(valor > 0 && valor<=saldo){
+            this.saldo-=valor;
+            return true;
+        }else
+            return false;
+    }
+
+    public boolean deposito(double valor){
+        if(valor>0){
+            this.saldo+=valor;
+            return true;
+        }else
+            return false;
+    }
+}
---
+++ Encapsulamento/src/TesteCC.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TesteCC {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        
+
+        input.close();
+    }
+}
