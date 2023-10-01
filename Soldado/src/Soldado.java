10c407e60f95d532df66637bf8247daaafc86d9f
Hygor Marques <68515062+Hygor0206@users.noreply.github.com>
2023-10-01 19:44:56 -0300

    Soldado class finished

--- Soldado/src/Soldado.java
+++ Soldado/src/Soldado.java
@@ -1,3 +1,34 @@
-public class Soldado {    
-    
+public class Soldado {
+    private static int numSoldados = 0;
+    private final String armaPadrao = "fuzil";
+    private final int distanciaPadrao = "10";
+    private String arma;
+    private double distanciaPadrao = 10.0;
+
+    public Soldado() {
+        numSoldados++;
+    }
+
+    public void mover(double distancia) {
+        if (distancia > 0) {
+            System.out.println("O soldado se moveu " + distancia + " metros.");
+        } else if(distancia==null){
+            System.out.println("O soldado se moveu " + distanciaPadrao + " metros.");
+        }else{
+            System.out.println("Distância inválida.");
+        }
+    }
+
+    public void atacar(String arma) {
+        if (numSoldados >= 10) {
+            if (arma == null) {
+                this.arma = armaPadrao;
+            } else {
+                this.arma = arma;
+            }
+            System.out.println("O soldado atacou com " + this.arma + ".");
+        } else {
+            System.out.println("Ainda não, esperando o exército ficar maior.");
+        }
+    }
 }
